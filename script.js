ecda607c84d0ca8ef7ba611e2cb8f66220133c50
Denys <47363351+Faceless22@users.noreply.github.com>
2019-04-04 13:24:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var sec = 3600;
+console.log(sec * 3);
+
+
+var num = 1;
+num += 12;
+num -= 14;
+num *=5;
+num /=7;
+num += 1;
+num -= 1;
+alert(num);
+
+
+a=10, b=2, c=5;
+console.log(a + b + c);
+
+
+a = 17, b = 10;
+c = a - b;
+d = 7;
+result = c + d;
+console.log(result);
+
+
+var name = ('Hello Denys!');
+alert(name);
