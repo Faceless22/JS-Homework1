50875d5d00a192a7f60dbb5daeb27aeebb8541b2
Denys <47363351+Faceless22@users.noreply.github.com>
2019-04-08 14:58:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var sec = 3600;
-console.log(sec * 3);
+var sec = 60;
+var hours = 3;
+var minutes = 60;
+console.log(sec * hours * minutes);
 
 
 var num = 1;
@@ -7,18 +9,16 @@ num += 12;
 num -= 14;
 num *=5;
 num /=7;
-num += 1;
-num -= 1;
+num ++;
+num --;
 alert(num);
 
 
-a=10, b=2, c=5;
+var a=10, b=2, c=5; 
 console.log(a + b + c);
 
 
-a = 17, b = 10;
-c = a - b;
-d = 7;
+var a = 17, b = 10; c = a - b; d = 7;
 result = c + d;
 console.log(result);
 
